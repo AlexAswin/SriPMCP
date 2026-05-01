8eaca2c2588c95f6fe8456d7b8d7b65557ffb497
Alex Aswin Rajappa <alexaswinrajappa@Alexs-MacBook-Air.local>
2026-05-01 03:40:47 -0400

    search vehicle by number fix

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyA_1vOEzZSs5W8ylsJqU1NwkApPwjRlw_c",
+        authDomain: "pm-cp-6d357.firebaseapp.com",
+        projectId: "pm-cp-6d357",
+        storageBucket: "pm-cp-6d357.firebasestorage.app",
+        messagingSenderId: "1082300166194",
+        appId: "1:1082300166194:web:e6b1020e9fc251fd744c2b"
+    }
+  };
